05d8d11a7b3dc392b870c7f4850cbf7b4163e5c7
Nikita <system518err@gmail.com>
2026-05-31 22:34:47 +0500

    версия 3: код программы распределен по отдельным методам: метод для парсинга, метод для вывода и выбора валют, метод для сохранения в xml и exel. методы реализованы для создания будущего интерфейса. Также добавлен метод для сохранения валют в эксель

--- src/CurrencyParserPackage/CurrencyParser.java
+++ src/CurrencyParserPackage/CurrencyParser.java
@@ -14,27 +14,93 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
+import java.io.FileOutputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class CurrencyParser {
-    static void main(String[] args) {
-        try {
-            String url = "https://www.cbr.ru/scripts/XML_daily.asp"; //путь к xml центробанка
+    public static void main(String[] args) {
 
-            String fileName = "currencies.xml"; //наименование файла куда будут сохранены данные
+        String url = "https://www.cbr.ru/scripts/XML_daily.asp"; //путь к xml центробанка
+        Document doc = parseUrl(url);
 
+        if (doc == null) {
+            System.out.println("XML-документ не был получен. Программа завершена.");
+            return;
+        }
+
+        Scanner in = new Scanner(System.in);
+
+        ArrayList<Element> selectedCurrencies = getCurrency(doc);
+
+        if (selectedCurrencies.isEmpty()) {
+            System.out.println("Валюты не выбраны. Программа завершена.");
+            return;
+        }
+
+        System.out.println();
+        System.out.println("Сохранить выбранные валюты в XML? y/n");
+        String xmlResponse = in.nextLine();
+
+        if (xmlResponse.equalsIgnoreCase("y")) {
+            createXml(selectedCurrencies);
+        }
+
+        System.out.println();
+        System.out.println("Сохранить выбранные валюты в Excel? y/n");
+        String excelResponse = in.nextLine();
+
+        if (excelResponse.equalsIgnoreCase("y")) {
+            createExcel(selectedCurrencies);
+        }
+
+    }
+
+    public static Document parseUrl(String url) {  //Метод для пасринга урл центробанка и запись результатов в document
+        try{
             System.out.println("Получение XML с сайта ЦБ РФ...");
-
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
-
             Document document = builder.parse(url);
             document.getDocumentElement().normalize();
 
             System.out.println("XML успешно получен.");
+            System.out.println("Корневой элемент: " + document.getDocumentElement().getNodeName());
 
-            System.out.println(document.getDocumentElement().getNodeName());
-            //System.out.println(document.getElementsByTagName("Name").item(10).getTextContent());
+            return document;
+        }
+        catch (Exception e){
+            System.out.println("Произошла ошибка при получении XML:");
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+
+
+    public static void createXml(ArrayList<Element> selectedCurrencies) {
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            Document newDocument = builder.newDocument();
+
+            Element root = newDocument.createElement("SelectedCurrencies");
+            newDocument.appendChild(root);
+
+            for (int i = 0; i < selectedCurrencies.size(); i++) {
+                Element oldValute = selectedCurrencies.get(i);
+
+                Node copiedValute = newDocument.importNode(oldValute, true);
+
+                root.appendChild(copiedValute);
+            }
 
             Transformer transformer = TransformerFactory.newInstance().newTransformer();
 
@@ -42,39 +108,35 @@ public class CurrencyParser {
             transformer.setOutputProperty(OutputKeys.METHOD, "xml");
             transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
 
-            DOMSource source = new DOMSource(document);
-            StreamResult result = new StreamResult(new File(fileName));
+            DOMSource source = new DOMSource(newDocument);
+            StreamResult result = new StreamResult(new File("currencies.xml"));
+
             transformer.transform(source, result);
 
-            System.out.println("Список валют сохранен в файле: " + fileName);
+            System.out.println("Выбранные валюты сохранены в XML-файл: currencies.xml");
 
-            System.out.println("Интересует курс конкретной валюты? y/n ");
-
-            Scanner in = new Scanner(System.in);
-            String response = in.nextLine();
-            if (response.equals("y")) {
-                NodeList valuteList = document.getElementsByTagName("Valute");
-                getCurrency(valuteList);
-            }
-
-        }
-        catch( Exception e) {
-            System.out.println("Произошла ошибка:");
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка при сохранении XML:");
             System.out.println(e.getMessage());
         }
+
     }
 
-    public static void getCurrency(NodeList valuteList){  //в метод передаем NodeList с валютами
+
+    public static ArrayList<Element> getCurrency(Document document){
+        ArrayList<Element> selectedCurrencies = new ArrayList<>();
+
+        NodeList valuteList = document.getElementsByTagName("Valute");
+
         System.out.println("Список доступных валют:");
         System.out.println("------------------------------");
 
-        for(int i = 0; i < valuteList.getLength(); i++){ //вывод всех валют для удобства
+        for (int i = 0; i < valuteList.getLength(); i++) {
             Node node = valuteList.item(i);
 
             Element valute = (Element) node;
 
             String charCode = valute.getElementsByTagName("CharCode").item(0).getTextContent();
-
             String name = valute.getElementsByTagName("Name").item(0).getTextContent();
 
             System.out.println(charCode + " - " + name);
@@ -85,53 +147,134 @@ public class CurrencyParser {
 
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Введите код валюты для поиска, например USD: ");
-        String userCode = in.nextLine().trim().toUpperCase();
+        System.out.print("Введите коды валют через пробел или ALL для выбора всех валют: ");
+        String userInput = in.nextLine().trim().toUpperCase();
 
-        boolean isFound = false;
+        if (userInput.equals("ALL")) {
 
-        for (int i = 0; i < valuteList.getLength(); i++) {  //поиск валюты введенной пользователем
-            Node node = valuteList.item(i);
+            for (int i = 0; i < valuteList.getLength(); i++) {
+                Node node = valuteList.item(i);
+                Element valute = (Element) node;
+                selectedCurrencies.add(valute);
+            }
 
-            Element valute = (Element) node;
+            System.out.println("Выбраны все валюты.");
 
-            String charCode = valute.getElementsByTagName("CharCode").item(0).getTextContent();
+        }
+        else {
+            String[] userCodes = userInput.split("\\s+");
 
-            if (charCode.equalsIgnoreCase(userCode)) {
+            for (int i = 0; i < userCodes.length; i++) {
+
+                String userCode = userCodes[i];
+                boolean isFound = false;
+
+                for (int j = 0; j < valuteList.getLength(); j++) {
+                    Node node = valuteList.item(j);
+
+                    Element valute = (Element) node;
+
+                    String charCode = valute.getElementsByTagName("CharCode").item(0).getTextContent();
+
+                    if (charCode.equalsIgnoreCase(userCode)) {
+                        selectedCurrencies.add(valute);
+
+                        String numCode = valute.getElementsByTagName("NumCode").item(0).getTextContent();
+                        String nominal = valute.getElementsByTagName("Nominal").item(0).getTextContent();
+                        String name = valute.getElementsByTagName("Name").item(0).getTextContent();
+                        String value = valute.getElementsByTagName("Value").item(0).getTextContent();
+
+                        System.out.println();
+                        System.out.println("Валюта выбрана:");
+                        System.out.println("------------------------------");
+                        System.out.println("Цифровой код: " + numCode);
+                        System.out.println("Буквенный код: " + charCode);
+                        System.out.println("Название: " + name);
+                        System.out.println("Номинал: " + nominal);
+                        System.out.println("Курс: " + value);
+                        System.out.println("------------------------------");
+
+                        isFound = true;
+                        break;
+                    }
+                }
+
+                if (!isFound) {
+                    System.out.println("Валюта с кодом " + userCode + " не найдена.");
+                }
+            }
+        }
+        return selectedCurrencies;
+
+    }
+
+    public static void createExcel(ArrayList<Element> selectedCurrencies){
+        try {
+            String excelFileName = "rates.xlsx";
+
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("Курсы валют");
+
+            Row header = sheet.createRow(0);
+            header.createCell(0).setCellValue("Дата");
+            header.createCell(1).setCellValue("Цифровой код");
+            header.createCell(2).setCellValue("Буквенный код");
+            header.createCell(3).setCellValue("Название");
+            header.createCell(4).setCellValue("Номинал");
+            header.createCell(5).setCellValue("Курс за номинал");
+            header.createCell(6).setCellValue("Курс за 1 единицу валюты");
+
+            int rowNumber = 1;
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+            String currentDate = LocalDateTime.now().format(formatter);
+
+            for (int i = 0; i < selectedCurrencies.size(); i++) {
+
+                Element valute = selectedCurrencies.get(i);
+
                 String numCode = valute.getElementsByTagName("NumCode").item(0).getTextContent();
+                String charCode = valute.getElementsByTagName("CharCode").item(0).getTextContent();
                 String nominal = valute.getElementsByTagName("Nominal").item(0).getTextContent();
                 String name = valute.getElementsByTagName("Name").item(0).getTextContent();
                 String value = valute.getElementsByTagName("Value").item(0).getTextContent();
 
-                System.out.println();
-                System.out.println("Валюта найдена:");
-                System.out.println("------------------------------");
-                System.out.println("Цифровой код: " + numCode);
-                System.out.println("Буквенный код: " + charCode);
-                System.out.println("Название: " + name);
-                System.out.println("Номинал: " + nominal);
-                System.out.println("Курс: " + value);
-                System.out.println("------------------------------");
+                double valueNumber = Double.parseDouble(value.replace(",", "."));
+                double rateForOneUnit = calculateRateForOneUnit(value, nominal);
 
-                //double rateForOneUnit = calculateRateForOneUnit(value, nominal);
-                //double res = calculateRubles(10, rateForOneUnit);
-                //System.out.println("10 " + charCode + " это: " + res + " руб.");
+                Row row = sheet.createRow(rowNumber);
 
-                isFound = true;
-                break;
+                row.createCell(0).setCellValue(currentDate);
+                row.createCell(1).setCellValue(numCode);
+                row.createCell(2).setCellValue(charCode);
+                row.createCell(3).setCellValue(name);
+                row.createCell(4).setCellValue(Integer.parseInt(nominal));
+                row.createCell(5).setCellValue(valueNumber);
+                row.createCell(6).setCellValue(rateForOneUnit);
+
+                rowNumber++;
             }
 
+            for (int i = 0; i <= 6; i++) { //выравнивание колонок по их длине
+               sheet.autoSizeColumn(i);
+            }
+
+            FileOutputStream outputStream = new FileOutputStream(excelFileName);
+
+            workbook.write(outputStream);
+
+            workbook.close();
+            outputStream.close();
+
+            System.out.println("Выбранные валюты сохранены в Excel-файл: " + excelFileName);
+
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка при создании Excel-файла:");
+            System.out.println(e.getMessage());
         }
-
-        if (!isFound) {
-            System.out.println();
-            System.out.println("Валюта с кодом " + userCode + " не найдена.");
-            System.out.println("Проверьте правильность введенного кода.");
-        }
-
-
 
     }
+
 
     public static double calculateRateForOneUnit(String value, String nominal) {   //метод для подсчета курса за 1 единицу валюты (для тех валют у которых курс более чем за 1 единицу, например, Иены)
         double valueNumber = Double.parseDouble(value.replace(",", "."));
@@ -151,6 +294,5 @@ public class CurrencyParser {
 
         return rubles / rateForOneUnit;
     }
-
 
 }
